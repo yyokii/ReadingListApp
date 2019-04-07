2dafa4e097a5b60c477499942ad066fce8bac058
yyokii <yyokii.h@gmail.com>
2019-04-08 00:30:58 +0900

    Initial Commit

---
+++ ReadingList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReadingList
+//
+//  Created by Yoki Higashihara on 2019/04/08.
+//  Copyright © 2019 Yoki Higashihara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
